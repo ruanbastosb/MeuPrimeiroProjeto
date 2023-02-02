8088360d6654606b7f10ee2e940ba00c9c21c138
ruanbastosb <ruanbastos77@gmail.com>
2023-02-02 15:22:11 -0300

    Meu Primeiro Projeto

---
+++ index.js
@@ -0,0 +1,25 @@
+let number = 3
+
+let secondNumber = 5
+
+let thirdNumber = 6
+
+let fourthNumber = 1
+
+let sum
+
+sum = number + secondNumber
+
+let multiplication = thirdNumber * number
+
+function showMessage() {
+    let name = document.getElementById('name').value
+
+    let age = document.getElementById('age').value
+
+    let text = 'Nome: ' + name + '\nIdade: ' + age
+
+    alert(text)
+}
+
+
